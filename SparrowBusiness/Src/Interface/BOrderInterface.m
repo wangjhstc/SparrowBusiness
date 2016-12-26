ba38c919c43dfa2075ffca3dcbbb01365cb76da8
wangjhstc <wangjhstc@126.com>
2016-12-26 16:09:28 +0800

    #feature:修改模型调用数据

--- SparrowBusiness/Src/Interface/BOrderInterface.m
+++ SparrowBusiness/Src/Interface/BOrderInterface.m
@@ -13,27 +13,19 @@
 
 - (NSArray<MCommodity *> *)fetchOrderWithTime:(long long)timeStamp pageSize:(NSInteger)pageSize pageIndex:(NSInteger)pageIndex {
     
-//    MCommodity *commodity = ({
-//        MCommodity *tempCommodity = [[MCommodity alloc] init];
-//        tempCommodity.name = @"清风茶臻纯品纸巾";
-//        tempCommodity.shelfLife = @"2017-09-30";
-//        tempCommodity.QSCode    = @"GB99722";
-//        tempCommodity.code      = 92228782739;
-//        tempCommodity;
-//    });
-//    
-//    MCommodity *commodity1 = ({
-//        MCommodity *tempCommodity = [[MCommodity alloc] init];
-//        tempCommodity.name = @"马克杯(FURIOUS7) - 580ML";
-//        tempCommodity.shelfLife = @"2019-09-30";
-//        tempCommodity.QSCode    = @"GB99442";
-//        tempCommodity.code      = 92228788886;
-//        tempCommodity;
-//    });
-//    
-//    return @[commodity,commodity1];
-
-    return nil;
+    MCommodity *commodity = ({
+        MCommodity *tempCommodity = [[MCommodity alloc] init];
+        tempCommodity.name = @"清风茶臻纯品纸巾";
+        tempCommodity;
+    });
+    
+    MCommodity *commodity1 = ({
+        MCommodity *tempCommodity = [[MCommodity alloc] init];
+        tempCommodity.name = @"马克杯(FURIOUS7) - 580ML";
+        tempCommodity;
+    });
+    
+    return @[commodity,commodity1];
 }
 
 - (NSArray<MCommodity *> *)loadAllCommodity {
